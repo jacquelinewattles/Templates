3c149e8d6b6eb29907c243e37bbc096782d4c7a0
jacquelinewattles <jacqueline.wattles@gmail.com>
2015-04-14 07:26:04 -0400

    cannot get list within array referencing to work

--- code/js/main.js
+++ code/js/main.js
@@ -1,5 +1,9 @@
 (function(){
 
+var templateHtml = $('#template').html();
+
+var templateFactory = _.template(templateHtml);
+
 function commaSeparateNumber(val){
     while (/(\d+)(\d{3})/.test(val.toString())){
       val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
@@ -7,21 +11,37 @@ function commaSeparateNumber(val){
     return val;
   }
 
-// Show slider value
-var sliderValue = $('#slider').on('input', function(){
+
+
+$('#slider').on('input', function(){
     var sliderVal = $( this ).val();
     var sliderValCom = commaSeparateNumber(sliderVal);
     $( '#sliderValue' ).html('$' + sliderValCom);
-    return sliderVal;
-	})
 
+        $.getJSON( "data/d1bball.json", function(myData) {
+            $.each(myData.TotRev, function(x, TotRevOf) {
 
-$.getJSON( "data/d1bball.json", function( myData ) {
+    	function selectSchools(){
+    	$( '#canvas' ).html('');
 
-  	// console.log(myData);
+    	var filteredRevs = _.where(TotRev, function(revs){ return revs < 7729000 });
+
+        console.log(filteredRevs);
+
+		filteredRevs.forEach(function(acceptSchool){
+				$('#canvas').append( templateFactory(acceptSchool) );
+		      });
+
+		// closes selectSchools function
+		}
+
+	return selectSchools();
+    
+    // closes each statements
+    });
+    // closes getJSON function and for loops
+    })
 })
-
-
 
 
 }).call(this);
